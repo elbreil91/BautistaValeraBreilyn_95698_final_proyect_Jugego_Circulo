a6d95ed367abd5fb5e28e63c4b55c46301a89605
Breilyn E. Bautista <elbril91@hotmail.com>
2021-02-13 16:19:25 -0600

    Crear clase

--- Juego_cirulo.py
+++ Juego_cirulo.py
@@ -3,64 +3,26 @@ from random import *
 import datetime
 
 
+class Juego_Circulo:
 
-
-def main():
-    w,h = 500,500
-    win = GraphWin('juego',w,h,autoflush=False)
-    win.setCoords(0,0,100,100)
-    p1,p2,rad = 15,15,5
-    intru = Text(Point(35,97),'Haga clik para en pesar y "q" + clik para salir.')
-    intru.setFill('blue')
-    intru.draw(win)
-
-
-
-
-
-    while True:
-        key = win.checkKey()
-        if key == 'q':
-            break
-
-        circulo = Circle(Point(p1, p2), rad)
-        circulo.setOutline('blue')
-        circulo.setWidth(2)
-        centro = circulo.getCenter()
-        if centro != w and h:
-            circulo.move(randint(0, 80), randint(0, 70))
-            circulo.draw(win)
-
-
-        shape = Circle(Point(0, 0), 4)
-        cie = win.getMouse()
-        cu = shape.getCenter()
-        rectx = cie.getX() - cu.getX()
-        recty = cie.getY() - cu.getY()
-        shape.setFill('green')
-        mov = shape.move(rectx, recty)
-        shape.draw(win)
-        update(4 / 4)
-        n = 60
-        secs = datetime.datetime.now()
-        tim = 60 - secs.second
-        te = Text(Point(98,98),tim)
-        te.draw(win)
-        update(4)
-        te.undraw()
-        
-
-        circulo.undraw()
-        update(4)
-        shape.undraw()
-
-    win.close()
-main()
+    def __init__(self):
+        self.win = GraphWin('Golpea al topo',400,400)
+        self.win.setCoords(0,0,100,100)
+        self.win.setBackground(color_rgb(176,146,111))
+        self.win.getMouse()
+        self.win.close()
 
 
 
 
 
+
+
+
+
+
+game = Juego_Circulo()
+game.run()
 
 
 
