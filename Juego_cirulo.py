0584d001bed727b2f15e7d7b2223bfe0d05cf3b8
Breilyn E. Bautista <elbril91@hotmail.com>
2021-02-15 19:13:25 -0600

    añadiendo funciones

--- Juego_cirulo.py
+++ Juego_cirulo.py
@@ -9,20 +9,38 @@ class Juego_Circulo:
         self.win = GraphWin('Golpea al topo',400,400)
         self.win.setCoords(0,0,100,100)
         self.win.setBackground(color_rgb(176,146,111))
-        self.win.getMouse()
-        self.win.close()
+
+
+
+    def topo(self):
+        for x in range(10):
+            cir = Circle(Point(10,10),5)
+            cir.setOutline('blue')
+            cir.setWidth(2)
+            cir.draw(self.win)
+            cir.move(randint(0,80),randint(0,80))
+        return cir
+
+
+    def cloucid(self):
+        key = self.win.getKey()
+
+        if key == "q":
+            self.win.close()
 
 
 
 
 
+def main():
+    game = Juego_Circulo()
+    game.topo()
+    game.cloucid()
 
 
+if __name__ =="__main__":
+    main()
 
-
-
-game = Juego_Circulo()
-game.run()
 
 
 
