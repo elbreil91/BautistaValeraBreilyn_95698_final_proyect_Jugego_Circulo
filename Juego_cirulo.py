f53078e21ff9b80f62f6bb06b630ae1009989582
Breilyn E. Bautista <elbril91@hotmail.com>
2021-02-15 23:15:34 -0600

    funciones principales

--- Juego_cirulo.py
+++ Juego_cirulo.py
@@ -1,5 +1,6 @@
 from graphics import *
 from random import *
+from time import *
 import datetime
 
 
@@ -8,18 +9,42 @@ class Juego_Circulo:
     def __init__(self):
         self.win = GraphWin('Golpea al topo',400,400)
         self.win.setCoords(0,0,100,100)
-        self.win.setBackground(color_rgb(176,146,111))
-
-
+        self.win.setBackground('green3')
 
     def topo(self):
-        for x in range(10):
+        keyt = 1
+        while keyt<=5:
+            keyt = keyt + 1
+
             cir = Circle(Point(10,10),5)
             cir.setOutline('blue')
             cir.setWidth(2)
-            cir.draw(self.win)
+            cen = cir.getCenter()
             cir.move(randint(0,80),randint(0,80))
-        return cir
+            update(4)
+            cir.draw(self.win)
+            update(4/3)
+            cir.undraw()
+
+
+
+    def Martillo(self):
+        t = 1
+        while t <= 5:
+            t = t + 1
+            shape = Circle(Point(0, 0), 4)
+            cie = self.win.getMouse()
+            cu = shape.getCenter()
+            rectx = cie.getX() - cu.getX()
+            recty = cie.getY() - cu.getY()
+            shape.setFill('green')
+            mov = shape.move(rectx, recty)
+            update(4)
+            shape.draw(self.win)
+            update(4/3)
+            shape.undraw()
+
+
 
 
     def cloucid(self):
@@ -30,13 +55,13 @@ class Juego_Circulo:
 
 
 
-
-
 def main():
-    game = Juego_Circulo()
-    game.topo()
-    game.cloucid()
 
+    game = Juego_Circulo()
+    
+    game.topo()
+    game.Martillo()
+    game.cloucid()
 
 if __name__ =="__main__":
     main()
