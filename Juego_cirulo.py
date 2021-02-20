e27a309c61565f5172c6befce6c38a603a449d21
Breilyn E. Bautista <elbril91@hotmail.com>
2021-02-19 20:58:17 -0600

    Game end

--- Juego_cirulo.py
+++ Juego_cirulo.py
@@ -1,66 +1,75 @@
 from graphics import *
 from random import *
-from time import *
 import datetime
-
 
 class Juego_Circulo:
 
     def __init__(self):
-        self.win = GraphWin('Golpea al topo',400,400)
+        self.win = GraphWin('Golpea al topo',500,400)
         self.win.setCoords(0,0,100,100)
         self.win.setBackground('green3')
 
+
+    def label(self):
+        tex = Text(Point(40,95),'Aga clik para "PLAY" y "q + clik" para salir')
+        tex.setFill('yellow2')
+        tex.setSize(13)
+        tex.setStyle("italic")
+        tex.draw(self.win)
+
     def topo(self):
-        keyt = 1
-        while keyt<=5:
-            keyt = keyt + 1
+        while True:
+            key = self.win.checkKey()
+            if key == 'q':
+                break
 
-            cir = Circle(Point(10,10),5)
-            cir.setOutline('blue')
-            cir.setWidth(2)
-            cen = cir.getCenter()
-            cir.move(randint(0,80),randint(0,80))
-            update(4)
-            cir.draw(self.win)
-            update(4/3)
-            cir.undraw()
+            circulo = Circle(Point(10, 10), 5)
+            circulo.setOutline('blue')
+            circulo.setWidth(2)
+            centro = circulo.getCenter()
+            if centro != self.win.getWidth() and self.win.getHeight():
+                circulo.move(randint(0, 70), randint(0, 70))
+                circulo.draw(self.win)
+                update(30)
 
-
-
-    def Martillo(self):
-        t = 1
-        while t <= 5:
-            t = t + 1
             shape = Circle(Point(0, 0), 4)
             cie = self.win.getMouse()
             cu = shape.getCenter()
             rectx = cie.getX() - cu.getX()
             recty = cie.getY() - cu.getY()
-            shape.setFill('green')
+            shape.setFill('red')
             mov = shape.move(rectx, recty)
-            update(4)
             shape.draw(self.win)
-            update(4/3)
+            update(30)
+            n = 60
+            secs = datetime.datetime.now()
+            tim = 60 - secs.second
+            te = Text(Point(98, 98), secs.second - 60)
+            te.draw(self.win)
+            update(4)
+            te.undraw()
+
+            circulo.undraw()
+            update(20)
             shape.undraw()
 
 
-
-
     def cloucid(self):
-        key = self.win.getKey()
+        key = self.win.checkKey()
 
         if key == "q":
-            self.win.close()
+            self.win.isClosed()
 
 
 
 def main():
 
     game = Juego_Circulo()
-    
+    game.label()
     game.topo()
-    game.Martillo()
+
+
+
     game.cloucid()
 
 if __name__ =="__main__":
